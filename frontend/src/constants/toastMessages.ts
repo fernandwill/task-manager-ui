d1d314e7e017b5ddcaa7e157113e64b812a75458
Fernand William <156734117+fernandwill@users.noreply.github.com>
2025-10-18 18:21:09 +0700

    Update task failure toast messages

--- frontend/src/constants/toastMessages.ts
+++ frontend/src/constants/toastMessages.ts
@@ -1,10 +1,10 @@
 export const NETWORK_RESTORED_MESSAGE = 'Network restored.';
 
 export const TASK_CREATE_FAILURE_TOAST_MESSAGE =
-  'Unable to create task. Please try again.';
+  'Failed to create task.';
 
 export const TASK_UPDATE_FAILURE_TOAST_MESSAGE =
-  'Unable to update task. Please try again.';
+  'Failed to update task.';
 
 export const TASK_DELETE_FAILURE_TOAST_MESSAGE =
-  'Unable to delete task. Please try again.';
+  'Failed to delete task.';
